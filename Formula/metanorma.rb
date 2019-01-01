ff82a0ef59d024ba004cecce37bff3b0a2b9adba
Alexande B <abobrikovich@gmail.com>
2019-01-01 19:52:53 +0300

    Refactor formula to keep all dependencies + update to gem version with fix of NODE_PATH variable

--- Formula/metanorma.rb
+++ Formula/metanorma.rb
@@ -1,47 +1,72 @@
+# frozen_string_literal: true
+
+require "language/node"
+
 class Metanorma < Formula
-  desc "Metanorma publishing toolchain"
+  desc "Toolchain for publishing metanorma documentation"
   homepage "https://www.metanorma.com"
-  url "https://github.com/riboseinc/metanorma-cli/archive/v1.0.10.tar.gz"
-  sha256 "6cbce57107af95730767dab7d1f246ffaef3ef82ab6a798c5d5fe5cb45211aca"
+  url "https://github.com/riboseinc/metanorma-cli/archive/v1.1.0.tar.gz"
+  # curl -sL https://github.com/riboseinc/metanorma-cli/archive/v1.1.0.tar.gz | shasum -a 256
+  sha256 "4ebbaa84d248376479082d9bdd4c01e6bf5464a81d59c441db7c9632a4286667"
 
-  bottle do
-    cellar :any_skip_relocation
-    rebuild 1
-    sha256 "4248d1886cd623bb95f05fdc6cad5c6579c5edb6545536e44e10dcb457722401" => :mojave
-    # sha256 "XXX" => :high_sierra
-    # sha256 "XXX" => :sierra
+  depends_on "node"
+  depends_on "plantuml"
+
+  resource "puppeteer" do
+    # required by 'metanorma-csd' gem
+    url "https://registry.npmjs.org/puppeteer/-/puppeteer-1.11.0.tgz"
+    sha256 "5ea179dd3ce90231dc70439876fd2a0b2abd04d7c30f588fb6e2a3b3459d0716"
   end
-
-  depends_on "node" => :required
-  depends_on "plantuml" => :optional
 
   def install
     ENV["GEM_HOME"] = libexec
-    system "npm", "install", "-g", "puppeteer"
+    ENV["ARCHFLAGS"] = "-arch x86_64"
+
+    # workaround to include fixed version of metanorma-standoc
+    system "sed", "-i", "--", "s/'metanorma-standoc', \"~> 1.0.6\"/'metanorma-standoc', \"1.1.0\"/g", "metanorma-cli.gemspec"
+
     system "gem", "build", "metanorma-cli.gemspec"
     system "gem", "install", "metanorma-cli-#{version}.gem"
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
+
+    resource("puppeteer").stage do
+      # Skip chromium download at install
+      # to avoid 'Failed changing dylib ID of **/libEGL.dylib' postpone chrome installation to first run
+      # we will continue chrome download on post_install
+      # https://discourse.brew.sh/t/how-to-prevent-dylib-from-linkage-fixing/3843
+      ENV["PUPPETEER_SKIP_CHROMIUM_DOWNLOAD"] = "1"
+      system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    end
+
+    bin.install Dir[libexec/"bin/metanorma"]
+    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"], :NODE_PATH => libexec/"lib/node_modules")
   end
 
-  ASCIIDOC_BLANK_HDR = <<~"HDR"
-    = Document title
-    Author
-    :docfile: test.adoc
-    :nodoc:
-    :novalid:
-    :no-isobib:
-
-  HDR
+  def post_install
+    cd libexec/"lib/node_modules/puppeteer/" do
+      npm_cache = *Language::Node.npm_cache_config
+      system "npm", "install", "--cache=#{npm_cache}" # rubocop:disable FormulaAudit/Miscellaneous
+    end
+  end
 
   test do
-    (testpath/"test.adoc").write(ASCIIDOC_BLANK_HDR)
-    system bin/"metanorma", "-iso", testpath/"test.adoc"
-    assert_predicate testpath/"test.xml", :exist?
-    assert_predicate testpath/"test.html", :exist?
+    METANORMA_TEST_DOC = <<~'ADOC'
+      = Document title
+      Author
+      :docfile: test.adoc
+      :nodoc:
+      :novalid:
+      :no-isobib:
+    ADOC
 
-    # (testpath/"test.adoc").write("= AsciiDoc is Writing Zen")
-    # system bin/"asciidoctor", "-b", "html5", "-o", "test.html", "test.adoc"
-    # assert_match "<h1>AsciiDoc is Writing Zen</h1>", File.read("test.html")
+    (testpath/"test-iso.adoc").write(METANORMA_TEST_DOC)
+    system bin/"metanorma", "--type", "iso", testpath/"test-iso.adoc"
+    assert_predicate testpath/"test-iso.xml", :exist?
+    assert_predicate testpath/"test-iso.html", :exist?
+
+    # blocked by issue https://github.com/riboseinc/homebrew-metanorma/pull/2#issuecomment-455129746
+    # (testpath/"test-csd.adoc").write(METANORMA_TEST_DOC)
+    # system bin/"metanorma", "--type", "csd", testpath/"test-csd.adoc"
+    # assert_predicate testpath/"test-csd.pdf", :exist?
+    # assert_predicate testpath/"test-csd.html", :exist?
   end
 end
