f69b3fac0a431efcbf01eb1f823a37dd7442e661
Alexande B <abobrikovich@gmail.com>
2019-06-03 23:30:55 +0300

    Add idnits xml2rfc tools

--- Formula/metanorma.rb
+++ Formula/metanorma.rb
@@ -1,22 +1,37 @@
 # frozen_string_literal: true
 
 require "language/node"
+require "language/python"
 
 class Metanorma < Formula
+  include Language::Python::Virtualenv
+
   desc "Toolchain for publishing metanorma documentation"
   homepage "https://www.metanorma.com"
   url "https://github.com/metanorma/metanorma-cli/archive/v1.1.8.tar.gz"
   # curl -sL https://github.com/metanorma/metanorma-cli/archive/v1.1.8.tar.gz | shasum -a 256
   sha256 "977345b1c0a00989cfed3de2ad47ef14013b7d5048cbc0ad5673aefafdc29db1"
-
+  
   depends_on "latexml"
   depends_on "node"
   depends_on "plantuml"
+  depends_on "python"
+  depends_on "ruby"
 
   resource "puppeteer" do
     # required by 'metanorma-csd' gem
     url "https://registry.npmjs.org/puppeteer/-/puppeteer-1.11.0.tgz"
     sha256 "5ea179dd3ce90231dc70439876fd2a0b2abd04d7c30f588fb6e2a3b3459d0716"
+  end
+
+  resource "idnits" do
+    url "https://files.pythonhosted.org/packages/source/i/idnits/idnits-3.0.0.tar.gz"
+    sha256 "ad1027cf2ac04139ca1116eb64f8bb7c0485f050e71f844b11e035a9e0d6ad68"
+  end
+
+  resource "xml2rfc" do
+    url "https://files.pythonhosted.org/packages/source/x/xml2rfc/xml2rfc-2.22.3.tar.gz"
+    sha256 "943bdb59c532be2ae4981d1cdaa56191e4d2fa0a79cf13125a8ac1a06b391e4e"
   end
 
   def install
@@ -39,8 +54,17 @@ class Metanorma < Formula
       system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     end
 
+    venv = virtualenv_create(libexec/"venv", "python3")
+    %w[idnits xml2rfc].each do |r|
+      venv.pip_install resource(r)
+    end
+
     bin.install Dir[libexec/"bin/metanorma"]
-    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"], :NODE_PATH => libexec/"lib/node_modules")
+    bin.env_script_all_files(libexec/"bin",
+      :PATH =>       "#{libexec/"bin"}:#{ENV["PATH"]}",
+      :GEM_HOME =>   ENV["GEM_HOME"],
+      :NODE_PATH =>  libexec/"lib/node_modules",
+      :PYTHONPATH => libexec/"venv/site-packages")
   end
 
   def post_install
