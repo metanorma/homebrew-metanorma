36fd0f85db3ccd029696aa967f1ec7df0986d70b
metanorma-ci <metanorma-ci@users.noreply.github.com>
2020-10-26 07:00:28 +0000

    Bump version to 1.3.8

--- Formula/metanorma-dev.rb
+++ Formula/metanorma-dev.rb
@@ -10,8 +10,8 @@ class MetanormaDev < Formula # rubocop:disable Metrics/ClassLength
   homepage "https://www.metanorma.com"
 
   # > formula-set-version.sh metanorma-cli #
-  url "https://github.com/metanorma/metanorma-cli/archive/v1.3.7.3.tar.gz"
-  sha256 "5d7d4fef997e525859bf3bf4ca7a958eb9037d1a2fdf21f7bce0b6479426ba1c"
+  url "https://github.com/metanorma/metanorma-cli/archive/v1.3.8.tar.gz"
+  sha256 "784aecc5920627bb64ec2da0a8601fb4898cf42f29e42d09eb45be84e15eaee4"
   # < formula-set-version.sh metanorma-cli #
 
   license "0BSD"
