557a10fbffb9b53eaa7454dca790f3d98683ad34
alex <alex-sc@users.noreply.github.com>
2025-05-31 15:44:19 +0300

    Test metanorma-src formula (brew/core)

---
+++ Formula/metanorma-src.rb
@@ -0,0 +1,60 @@
+class Metanorma < Formula
+  include Language::Python::Virtualenv
+
+  desc "Toolchain for publishing metanorma documentation"
+  homepage "https://www.metanorma.com"
+
+  url "https://rubygems.org/downloads/metanorma-cli-1.12.8.gem"
+  sha256 "b746637b00a051ca409ccf98fc82c12c443d27183c3d4bdd3535ad47eb015573"
+
+  license "0BSD"
+
+  depends_on "gflags"
+  depends_on "graphviz"
+  depends_on "openjdk"
+  depends_on "plantuml"
+  depends_on "ruby"
+  depends_on "xml2rfc" # required by 'metanorma-ietf' gem
+
+  on_linux do
+    depends_on "libxslt"
+  end
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    system "gem", "install", cached_download, "--no-document"
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  def caveats
+    <<~EOS
+      inkscape >= 1.0 is required to generate Word output using SVG images.
+      Install it by running `brew cask install inkscape` or
+      directly download from https://inkscape.org/release/inkscape-1.0/
+    EOS
+  end
+
+  test do
+    test_doc = <<~ADOC
+      = Document title
+      Author
+      :docfile: test.adoc
+      :nodoc:
+      :novalid:
+      :no-isobib:
+    ADOC
+
+    (testpath / "test-iso.adoc").write(test_doc)
+    system bin / "metanorma", "--type", "iso", testpath / "test-iso.adoc",
+           "--agree-to-terms"
+    assert_path_exists testpath / "test-iso.xml"
+    assert_path_exists testpath / "test-iso.html"
+
+    (testpath / "test-csa.adoc").write(test_doc)
+    system bin / "metanorma", "--type", "csa", testpath / "test-csa.adoc",
+           "--agree-to-terms"
+    assert_path_exists testpath / "test-csa.pdf"
+    assert_path_exists testpath / "test-csa.html"
+  end
+end
