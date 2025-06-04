01d1f70ba0facaaa7eb440af49fdeac1e32d8ff4
alex <alex-sc@users.noreply.github.com>
2025-06-04 19:35:02 +0300

    Fixes

--- Formula/metanorma.rb
+++ Formula/metanorma.rb
@@ -1,74 +1,54 @@
 class Metanorma < Formula
   desc "Toolchain for publishing metanorma documentation"
   homepage "https://www.metanorma.com"
-
   url "https://rubygems.org/downloads/metanorma-cli-1.12.8.gem"
   sha256 "b746637b00a051ca409ccf98fc82c12c443d27183c3d4bdd3535ad47eb015573"
-
   license "0BSD"
 
   depends_on "pkgconf" => :build
-  # depends_on "cmake"
-  # depends_on "make"
-  # depends_on "gcc"
   depends_on "gflags"
-  #depends_on "graphviz"
-  #depends_on "openjdk"
-  #depends_on "plantuml"
+  depends_on "graphviz"
+  depends_on "openjdk"
+  depends_on "plantuml"
   depends_on "readline"
   depends_on "ruby@3.3"
-  depends_on "sqlite"
   depends_on "xml2rfc"
 
-  uses_from_macos "zlib"
+  uses_from_macos "sqlite"
 
   on_linux do
     depends_on "libxslt"
-    depends_on "zlib"
-  end
-
-  def delete_selected_gems(gem_names)
-    gem_names.each do |gem_name|
-      gem_dir = Pathname.glob("#{libexec}/gems/#{gem_name}-*").first
-      puts "Deleting #{gem_name}"
-      rm_r(gem_dir)
-    end
   end
 
   def install
     ENV["GEM_HOME"] = libexec
     ENV["GEM_PATH"] = libexec
 
-    # Ensure Homebrew's libsqlite is found before the system version.
-    #sqlite = Formula["sqlite"]
-    #ENV.append "LDFLAGS", "-L#{sqlite.opt_lib}"
-    #ENV.append "CFLAGS", "-I#{sqlite.opt_include}"
+    if OS.linux?
+      # Install sqlite3 with brew's libsqlite3
+      system "gem", "install", "sqlite3", "-v", "1.7.3", "--no-document",
+             "--platform=ruby",
+             "--",
+             "--enable-system-libraries",
+             "--with-sqlite3-include=#{Formula["sqlite"].opt_include}",
+             "--with-sqlite3-lib=#{Formula["sqlite"].opt_lib}"
 
-    puts(Formula["sqlite"].opt_lib/shared_library("libsqlite3"))
+      # Install pngcheck with brew's zlib (libz.so.1)
+      ENV.append "CFLAGS", "-I$(brew --prefix zlib)/include"
+      ENV.append "LDFLAGS", "-L$(brew --prefix zlib)/lib -Wl,-rpath,$(brew --prefix zlib)/lib"
+      ENV.append "PKG_CONFIG_PATH", "$(brew --prefix zlib)/lib/pkgconfig"
+      system "gem", "install", "pngcheck", "--no-document", "--platform=ruby"
+    end
 
-    system "gem", "install", "sqlite3", "-v", "1.7.3", "--no-document", "--platform=ruby", "--force",
-           "--",
-           "--enable-system-libraries",
-           "--with-sqlite3-include=" + Formula["sqlite"].opt_include, "--with-sqlite3-lib=" + Formula["sqlite"].opt_lib
-
-    ENV.append "CFLAGS", "-I$(brew --prefix zlib)/include"
-    ENV.append "LDFLAGS", "-L$(brew --prefix zlib)/lib -Wl,-rpath,$(brew --prefix zlib)/lib"
-    ENV.append "PKG_CONFIG_PATH", "$(brew --prefix zlib)/lib/pkgconfig"
-
-    system "gem", "install", "pngcheck", "--no-document", "--platform=ruby"
-
+    # Install metanorma
     system "gem", "install", cached_download, "--no-document"
-
-    system "gem", "contents", "sqlite3"
-
-    #system "find", libexec.to_s, "-type", "f"
 
     bin.install Dir["#{libexec}/bin/metanorma"]
     bin.env_script_all_files(
       libexec/"bin",
       PATH:      [libexec/"bin", "$PATH"].join(":"),
       GEM_HOME:  ENV["GEM_HOME"],
-      JAVA_HOME: Language::Java.java_home("1.8+"),
+      JAVA_HOME: ENV["JAVA_HOME"] || Language::Java.java_home("1.8+"),
       )
   end
 
