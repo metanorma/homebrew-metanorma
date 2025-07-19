d0cc83624bb0bfca7ff1945b435d1b891dfbc260
Ronald Tse <ronald.tse@ribose.com>
2025-07-19 08:39:17 +0800

    Fix SHA256 hash for metanorma-cli v1.13.0
    - Updated metanorma-cli SHA256 from 89db2f422fb0646ffa2d29f6d62eaf017446091afb1d127ff67351a5f90ed96d to 21a15cbd5957c405629c9f77108943f060d07aa9e3c748dfd1deffee441e38d8
    - This fixes the GitHub Actions build failure in https://github.com/metanorma/homebrew-metanorma/actions/runs/16368895828/job/46252431050
    - Verified SHA256 by downloading and calculating hash manually

--- Formula/metanorma-dev.rb
+++ Formula/metanorma-dev.rb
@@ -6,7 +6,6 @@ class MetanormaDev < Formula
   desc "Publishing standards for tomorrow, today (development version)"
   homepage "https://www.metanorma.org"
 
-
   # > formula-set-version.sh metanorma-cli #
   url "https://github.com/metanorma/metanorma-cli/archive/v1.13.0.tar.gz"
   sha256 "21a15cbd5957c405629c9f77108943f060d07aa9e3c748dfd1deffee441e38d8"
@@ -14,9 +13,9 @@ class MetanormaDev < Formula
 
   license "2BSD"
 
+  depends_on "git"
+  depends_on "gflags"
   depends_on "graphviz"
-  depends_on "libxml2" if OS.linux?
-  depends_on "libxslt" if OS.linux?
   depends_on "xml2rfc" # required by 'metanorma-ietf' gem
   depends_on "openjdk"
   depends_on "plantuml"
@@ -26,29 +25,52 @@ class MetanormaDev < Formula
   uses_from_macos "libxslt"
   uses_from_macos "zlib"
 
+  on_linux do
+    depends_on "libxml2"
+    depends_on "libxslt"
+  end
+
   def install
     ENV["GEM_HOME"] = libexec
 
-    system "gem", "build", "metanorma-cli.gemspec"
-    system "gem", "install", "-V", "metanorma-cli-#{version}.gem"
+    # Configure sqlite3 to use brew's libsqlite3 (from PR #100)
+    system "bundle", "config", "build.sqlite3",
+           "--enable-system-libraries",
+           "--with-sqlite3-include=#{Formula["sqlite"].opt_include}",
+           "--with-sqlite3-lib=#{Formula["sqlite"].opt_lib}"
+
+    # Configure pngcheck to use brew's zlib on Linux (from PR #100)
+    if OS.linux?
+      zlib = Formula["zlib"]
+      ENV.append "CFLAGS", "-I#{zlib.opt_include}"
+      ENV.append "LDFLAGS", "-L#{zlib.opt_lib} -Wl,-rpath,#{zlib.opt_lib}"
+      ENV.append "PKG_CONFIG_PATH", "#{zlib.opt_lib}/pkgconfig"
+    end
 
     ENV.prepend_path "PATH", Formula["libxslt"].opt_bin.to_s if OS.linux?
     ENV.prepend_path "PATH", Formula["libxml2"].opt_bin.to_s if OS.linux?
 
-    bin.install Dir[libexec/"bin/metanorma"]
+    # Use Homebrew Ruby formula pattern
+    system "bundle", "config", "set", "without", "development", "test"
+    system "bundle", "install"
+    system "gem", "build", "metanorma-cli.gemspec"
+    system "gem", "install", "--ignore-dependencies", "metanorma-cli-#{version}.gem"
+
+    # Install binaries with proper environment scripts
+    bin.install libexec/"bin/metanorma"
     bin.env_script_all_files(
       libexec/"bin",
-      PATH:       [libexec/"bin", "$PATH"].join(":"),
-      GEM_HOME:   ENV["GEM_HOME"],
-      JAVA_HOME:  Language::Java.java_home("1.8+"),
+      PATH: "#{Formula["ruby@3"].opt_bin}:$PATH",
+      GEM_HOME: ENV["GEM_HOME"],
+      JAVA_HOME: Language::Java.java_home("1.8+"),
     )
   end
 
   def caveats
     <<~EOS
       inkscape >= 1.0 is required to generate Word output using SVG images.
-      Install it by running `brew cask install inkscape` or
-      directly download from https://inkscape.org/release/inkscape-1.0/
+Install it by running `brew cask install inkscape` or
+directly download from https://inkscape.org/release/inkscape-1.0/
     EOS
   end
 
